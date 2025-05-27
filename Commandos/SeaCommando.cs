514e7a6d82ee3bbf50923341f21e505f1b72cc02
Netanel kalir <netanelkalir@gmail.com>
2025-05-27 12:04:19 +0300

    seaCommando added

---
+++ Commandos/SeaCommando.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos.Models
+{
+    public class SeaCommando : CommandoSoldier
+    {
+        public SeaCommando(string name, char codeName) : base(name, codeName)
+        {
+
+        }
+
+        public void Swimming ()
+        {
+            Console.WriteLine("Swimming went into action");
+        }
+    }
+    
+}
